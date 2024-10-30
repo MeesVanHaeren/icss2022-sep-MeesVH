4e871173bd0fe3838a8c788e442052ac9c83a7e7
Mees <m.vanhaeren@student.han.nl>
2024-10-30 12:11:06 +0100

    tweaked code for redundant method

--- startcode/src/main/java/nl/han/ica/icss/ast/IfClause.java
+++ startcode/src/main/java/nl/han/ica/icss/ast/IfClause.java
@@ -78,18 +78,4 @@ public class IfClause extends ASTNode {
         return conditionalExpression;
     }
     public ElseClause getElseClause() { return elseClause; }
-
-    //Meesmade method. This method adds an else to the lowest if it can find under this clause, which does include this clause as well
-    public void appendElse(ElseClause elseClause) {
-        //Gets all ifClauses in the body
-        List<ASTNode> ifClauses = body.stream().filter(astNode -> astNode instanceof IfClause).collect(Collectors.toList());
-        if (ifClauses.isEmpty()){
-            //Basecase! If there are no ifClauses in the body else is put in elseClause
-            this.elseClause = elseClause;
-        } else {
-            //If there IS more than none, get the last, and call this method on it.
-            IfClause lastIfClause = (IfClause) ifClauses.get(ifClauses.size()-1);
-            lastIfClause.appendElse(elseClause);
-        }
-    }
 }
